45f832f89bcdaea9b8d19da2fe85724cf2ddafe4
Timo <tr110@hdm-stuttgart.de>
2024-03-03 18:43:27 +0100

    Added answer for exercise 1.5

---
+++ part1/part1.5/src/App.jsx
@@ -0,0 +1,59 @@
+const Header = (props) => {
+  console.log(props)
+  return (
+    <>
+      <h1>{props.title}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <>
+      <p>{props.sections[0].name} {props.sections[0].exercises}</p>
+      <p>{props.sections[1].name} {props.sections[1].exercises}</p>
+      <p>{props.sections[2].name} {props.sections[2].exercises}</p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <>
+      <p>Number of exercises {props.sections[0].exercises + props.sections[1].exercises + props.sections[2].exercises}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+
+    ]
+  }
+
+  return (
+    <div>
+      <Header title={course.name} />
+      <Content sections={course.parts} />
+      <Total sections={course.parts} />
+    </div>
+  )
+}
+
+export default App
