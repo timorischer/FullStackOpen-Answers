61431ed5d4d11717cc234f4f30cf338331505c46
Timo <tr110@hdm-stuttgart.de>
2024-03-19 19:43:25 +0100

    Added answer for exercise 2.7

---
+++ part2/part2.7/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const App = (props) => {
+  const [persons, setPersons] = useState([{name: 'Arto Hellas'}]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+  
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    if (
+      persons.filter((person) => JSON.stringify(person.name) === newName)) {
+        alert(`${newName} is already added to the phonebook`)
+      } 
+    else {
+      const nameObject = {
+      name: newName
+      }
+      setPersons(persons.concat(nameObject))
+      setNewName('')
+      console.log('persons is now:', persons)
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+            value={newName}
+            onChange={handleNameChange} 
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>{persons.map(person => <p key={person.name}>{person.name}</p>)}</div>
+    </div>
+  )
+}
+
+export default App
