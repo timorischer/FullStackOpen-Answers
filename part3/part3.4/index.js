948e298153ec60127eda39540b2550674caf023f
Timo <tr110@hdm-stuttgart.de>
2024-09-09 11:53:19 +0200

    Added answer for exercise 3.4

---
+++ part3/part3.4/index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    { 
+        "id": "2",
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+    },
+    { 
+        "id": "3",
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+    },
+    { 
+        "id": "4",
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+    }
+]
+
+app.get('/info', (request, response) => {
+    response.send(
+        `<p>Phonebook has info for ${persons.length} people</p>
+        <p>${Date()}</p>`)
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
