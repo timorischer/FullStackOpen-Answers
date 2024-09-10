961e3473437f4162066d5c10fc88b4cb7ab423f0
Timo <tr110@hdm-stuttgart.de>
2024-09-10 16:12:10 +0200

    added locally working frontend production build

--- part3/backend_test1/index.js
+++ part3/backend_test1/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json())
+const cors = require('cors')
 
 let notes = [
     {
@@ -19,6 +19,23 @@ let notes = [
         important: true
     }
 ]
+
+const requestLogger = (request, response, next) => {
+    console.log('Method', request.method)
+    console.log('Path: ', request.path)
+    console.log('Body: ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(express.json())
+app.use(express.static('dist'))
+app.use(cors())
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -74,6 +91,9 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
